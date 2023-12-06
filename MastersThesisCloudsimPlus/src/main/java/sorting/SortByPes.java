c3e625eab4b0081c2bde1e600a0bfeb6af242d1b
Vibhu <vibhu96@gmail.com>
2023-12-06 13:49:05 -0800

    Best Fit / Worst Fit Scheduler helper

---
+++ MastersThesisCloudsimPlus/src/main/java/sorting/SortByPes.java
@@ -0,0 +1,25 @@
+package sorting;
+import java.util.Comparator;
+
+import org.cloudsimplus.vms.Vm;
+
+public class SortByPes implements Comparator<Vm> {
+    public int compare(Vm a, Vm b)
+    {
+        if (a.getPesNumber() < b.getPesNumber())
+        {
+            return -1;
+        }
+        else if (a.getPesNumber() > b.getPesNumber())
+        {
+            return 1;
+        }
+        else if (a.getPesNumber() == a.getPesNumber())
+        {
+            return 0;
+        }
+        else {
+        	return 0;
+        }
+    }
+}
