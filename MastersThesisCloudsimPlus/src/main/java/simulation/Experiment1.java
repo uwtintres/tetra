88cb0c1b3b18e82cf0a46b20f12a97216574e9f9
Vibhu <vibhu96@gmail.com>
2023-12-30 18:02:52 +0530

    Added additional experiments

--- MastersThesisCloudsimPlus/src/main/java/simulation/Experiment1.java
+++ MastersThesisCloudsimPlus/src/main/java/simulation/Experiment1.java
@@ -19,21 +19,43 @@ public class Experiment1 {
 	public static void main(String[] args) throws InterruptedException
 	{
 		
-		for (int i = 10; i <= 10; i += 10 )
+		for (int i = 100; i <= 1000; i += 100 )
 		{
-			// base case - FCFS
-			FirstFitSimulationFiltrationRandom exp = new FirstFitSimulationFiltrationRandom(3,i,1,0.4);
+			// Experiment 1 - base case - FCFS
+			FirstFitSimulationFiltrationRandom exp = new FirstFitSimulationFiltrationRandom(1,i,4,0.4);
 			
-			// base case 2 - Best Fit
-			BestWorstFitSimulationFiltration exp2 = new BestWorstFitSimulationFiltration(3,i,1,0.4,"best_fit");
+			// Experiment 1 - base case 2 - Best Fit
+			BestWorstFitSimulationFiltration exp2 = new BestWorstFitSimulationFiltration(1,i,4,0.4,"best_fit");
 			
-			// base case 3 - Worst Fit
-			BestWorstFitSimulationFiltration exp3 = new BestWorstFitSimulationFiltration(3,i,1,0.4,"worst_fit");
+			// Experiment 1 - base case 3 - Worst Fit
+			BestWorstFitSimulationFiltration exp3 = new BestWorstFitSimulationFiltration(1,i,4,0.4,"worst_fit");
 			
+			// Experiment 1 - optimized scheduler using TOPSIS
+			FirstFitSimulation_TopsisVmFiltration exp4 = new FirstFitSimulation_TopsisVmFiltration(1,i,4,0.4);
 			
-			// optimized scheduler using TOPSIS
-			FirstFitSimulation_TopsisVmFiltration exp4 = new FirstFitSimulation_TopsisVmFiltration(3,i,1,0.4);
+			// Experiment 2 - base case - FCFS
+			FirstFitSimulationFiltrationRandom exp5 = new FirstFitSimulationFiltrationRandom(2,i,2,0.4);
 			
+			// Experiment 2 - base case 2 - Best Fit
+			BestWorstFitSimulationFiltration exp6 = new BestWorstFitSimulationFiltration(2,i,2,0.4,"best_fit");
+			
+			// Experiment 2 - base case 3 - Worst Fit
+			BestWorstFitSimulationFiltration exp7 = new BestWorstFitSimulationFiltration(2,i,2,0.4,"worst_fit");
+			
+			// Experiment 2 - optimized scheduler using TOPSIS
+			FirstFitSimulation_TopsisVmFiltration exp8 = new FirstFitSimulation_TopsisVmFiltration(2,i,2,0.4);
+			
+			// Experiment 3 - base case - FCFS
+			FirstFitSimulationFiltrationRandom exp9 = new FirstFitSimulationFiltrationRandom(3,i,1,0.4);
+			
+			// Experiment 3 - base case 2 - Best Fit
+			BestWorstFitSimulationFiltration exp10 = new BestWorstFitSimulationFiltration(3,i,1,0.4,"best_fit");
+			
+			// Experiment 3 - base case 3 - Worst Fit
+			BestWorstFitSimulationFiltration exp11 = new BestWorstFitSimulationFiltration(3,i,1,0.4,"worst_fit");
+			
+			// Experiment 3 - optimized scheduler using TOPSIS
+			FirstFitSimulation_TopsisVmFiltration exp12 = new FirstFitSimulation_TopsisVmFiltration(3,i,1,0.4);
 		}			
 	}
 }
