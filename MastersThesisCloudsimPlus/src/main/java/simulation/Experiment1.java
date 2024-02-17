420da297b7139ddd86e7f17a7687777db85f0911
Vibhu <vibhu96@gmail.com>
2024-02-17 13:16:50 -0800

    Updated comments

--- MastersThesisCloudsimPlus/src/main/java/simulation/Experiment1.java
+++ MastersThesisCloudsimPlus/src/main/java/simulation/Experiment1.java
@@ -21,37 +21,37 @@ public class Experiment1 {
 		
 		for (int i = 100; i <= 1000; i += 100 )
 		{
-			// Experiment 1 - base case - FCFS
+			// Experiment 1 - low competition - FCFS
 			FirstFitSimulationFiltrationRandom exp = new FirstFitSimulationFiltrationRandom(1,i,4,0.4);
 			
-			// Experiment 1 - base case 2 - Best Fit
+			// Experiment 1 - low competition - Best Fit
 			BestWorstFitSimulationFiltration exp2 = new BestWorstFitSimulationFiltration(1,i,4,0.4,"best_fit");
 			
-			// Experiment 1 - base case 3 - Worst Fit
+			// Experiment 1 - low competition - Worst Fit
 			BestWorstFitSimulationFiltration exp3 = new BestWorstFitSimulationFiltration(1,i,4,0.4,"worst_fit");
 			
 			// Experiment 1 - optimized scheduler using TOPSIS
 			FirstFitSimulation_TopsisVmFiltration exp4 = new FirstFitSimulation_TopsisVmFiltration(1,i,4,0.4);
 			
-			// Experiment 2 - base case - FCFS
+			// Experiment 2 - medium competition - FCFS
 			FirstFitSimulationFiltrationRandom exp5 = new FirstFitSimulationFiltrationRandom(2,i,2,0.4);
 			
-			// Experiment 2 - base case 2 - Best Fit
+			// Experiment 2 - medium competition - Best Fit
 			BestWorstFitSimulationFiltration exp6 = new BestWorstFitSimulationFiltration(2,i,2,0.4,"best_fit");
 			
-			// Experiment 2 - base case 3 - Worst Fit
+			// Experiment 2 - medium competition - Worst Fit
 			BestWorstFitSimulationFiltration exp7 = new BestWorstFitSimulationFiltration(2,i,2,0.4,"worst_fit");
 			
 			// Experiment 2 - optimized scheduler using TOPSIS
 			FirstFitSimulation_TopsisVmFiltration exp8 = new FirstFitSimulation_TopsisVmFiltration(2,i,2,0.4);
 			
-			// Experiment 3 - base case - FCFS
+			// Experiment 3 - high competition - FCFS
 			FirstFitSimulationFiltrationRandom exp9 = new FirstFitSimulationFiltrationRandom(3,i,1,0.4);
 			
-			// Experiment 3 - base case 2 - Best Fit
+			// Experiment 3 - high competition - Best Fit
 			BestWorstFitSimulationFiltration exp10 = new BestWorstFitSimulationFiltration(3,i,1,0.4,"best_fit");
 			
-			// Experiment 3 - base case 3 - Worst Fit
+			// Experiment 3 - high competition - Worst Fit
 			BestWorstFitSimulationFiltration exp11 = new BestWorstFitSimulationFiltration(3,i,1,0.4,"worst_fit");
 			
 			// Experiment 3 - optimized scheduler using TOPSIS
